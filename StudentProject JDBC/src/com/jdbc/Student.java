c8f5030d0c3bfe3d380f6c1583dd765c2d08de82
Shubham Tadas <shubhamtadas828@gmail.com>
2022-02-28 09:47:34 +0530

    updated 8th and 10th case

--- StudentProject JDBC/src/com/jdbc/Student.java
+++ StudentProject JDBC/src/com/jdbc/Student.java
@@ -1,5 +1,6 @@
 package com.jdbc;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -78,9 +79,8 @@ public class Student {
 			ResultSet rs=smt.executeQuery(q);
 			
 			if(rs.next()){ 
-				do{
 				System.out.println("Total number of students in 'P01': "+rs.getInt(1));
-				}while(rs.next());
+			
 			}
 			else{
 				System.out.println("Record Not Found...");
@@ -209,28 +209,46 @@ public class Student {
 	
 	public void case8() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection(JdbcURL, Username, password);
-			Statement smt=con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
-
-			String q= "select st_no,COUNT(designation) from StudentProject group by st_no,prj_no";
-			//to execute query
-			ResultSet rs=smt.executeQuery(q);
-			
-			HashMap<String, Integer> hm = new HashMap<String, Integer>();
-			if(rs.next()){ 
-				hm.put(rs.getString(1), rs.getInt(2));
+		String s1 = "select st_no,count(distinct (designation)) from studentproject group by st_no,prj_no;";
+		Connection conn = DriverManager.getConnection(JdbcURL, Username, password);
+		Statement st1 = conn.createStatement();
+		ResultSet rs1 = st1.executeQuery(s1);
+		HashMap <String,Integer> l = new HashMap <String,Integer>();
+		int flag=1;
+		String a = null;
+		int b = 0;
+		while(rs1.next())
+		{
+			if(flag==1)
+			{
+				 a= rs1.getString(1);
+				 b = rs1.getInt(2);
+				 l.put(a, b);
+				flag=0;
 			}
-			else{
-				System.out.println("Record Not Found...");
-			}
-			int maxDesignation = (Collections.max(hm.values()));
-			for (Entry<String, Integer> entry : hm.entrySet()) {
-				if(entry.getValue()==maxDesignation) {
-					System.out.println(entry.getKey()+" "+entry.getValue());
+			else if(a.equalsIgnoreCase(rs1.getString(1)) && flag==0)
+			{
+				if(b<rs1.getInt(2))
+				{
+					b=rs1.getInt(2);
+					l.replace(a, b);
 				}
+				flag=1;
+				
 			}
-			con.close();
+			else
+			{
+				l.put(rs1.getString(1),rs1.getInt(2));
+			}
+				
+		}
+		int maxValueInMap=(Collections.max(l.values()));  
+        for (java.util.Map.Entry<String, Integer> entry : l.entrySet()) {  
+            if (entry.getValue()==maxValueInMap) {
+                System.out.println(entry.getKey() + "\t" +entry.getValue());
+              
+            }
+        }
 		}
 		catch (Exception e) {
 			e.printStackTrace();
